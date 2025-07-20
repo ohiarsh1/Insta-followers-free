7b398d2488397762cbc2c027927d909a7d5eca65
ohiarsh1 <ohiarsh07@gmail.com>
2025-07-20 17:58:46 +0530

    index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/location', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use(express.static(__dirname));
