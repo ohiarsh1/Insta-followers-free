fb47dd18c25c0db2a089e54d013f80797eda1651
ohiarsh1 <ohiarsh07@gmail.com>
2025-08-02 17:21:48 +0530

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 30001;
 
 app.use(bodyParser.json());
 
